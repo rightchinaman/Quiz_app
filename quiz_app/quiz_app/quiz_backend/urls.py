77540858cdb2f177e0eca53b02cc1dcaa7b67a0b
Tanmay Rajan <143349394+rightchinaman@users.noreply.github.com>
2024-12-14 12:16:13 +0530

    Delete quiz_app/quiz_app/quiz_backend directory

--- quiz_app/quiz_app/quiz_backend/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('start/', views.start_quiz, name='start_quiz'),
-    path('question/', views.get_question, name='get_question'),
-    path('submit/', views.submit_answer, name='submit_answer'),
-    path('summary/', views.quiz_summary, name='quiz_summary'),
-]
